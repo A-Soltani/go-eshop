2ac54c8048a63391262002fa733204b7682b84f8
A-Soltani <soltani1384@gmail.com>
2021-11-10 22:04:15 +0330

    move AddOrderItem to order file

--- src/services/ordering/domain/order-aggregate/order.go
+++ src/services/ordering/domain/order-aggregate/order.go
@@ -1,5 +1,7 @@
 package domain
 
+import "github.com/A-Soltani/go-eshop/src/services/ordering/domain/errors"
+
 type OrderStatus int
 
 const (
@@ -8,12 +10,28 @@ const (
 )
 
 type Order struct {
-	Id     int
-	Status OrderStatus
+	Id         int
+	Status     OrderStatus
+	OrderItems []OrderItem
 }
 
 func AddOrder() *Order {
 	o := &Order{}
 	o.Status = OrderSubmitted
 	return o
+}
+
+func AddOrderItem(productId int, units int, unitPrice float64, discount float64) (*OrderItem, error) {
+	if productId < 1 {
+		return nil, &errors.OrderingDomainError{Msg: "productId should be greater than 1"}
+	}
+	if float64(units)*unitPrice < discount {
+		return nil, &errors.OrderingDomainError{Msg: "The total of order item is lower than applied discount"}
+	}
+	oi := &OrderItem{
+		productId: productId,
+		units:     units,
+		unitPrice: unitPrice,
+		discount:  discount}
+	return oi, nil
 }
--- src/services/ordering/domain/order-aggregate/order_item.go
+++ src/services/ordering/domain/order-aggregate/order_item.go
@@ -9,21 +9,6 @@ type OrderItem struct {
 	discount  float64
 }
 
-func AddOrderItem(productId int, units int, unitPrice float64, discount float64) (*OrderItem, error) {
-	if productId < 1 {
-		return nil, &errors.OrderingDomainError{Msg: "productId should be greater than 1"}
-	}
-	if float64(units)*unitPrice < discount {
-		return nil, &errors.OrderingDomainError{Msg: "The total of order item is lower than applied discount"}
-	}
-	oi := &OrderItem{
-		productId: productId,
-		units:     units,
-		unitPrice: unitPrice,
-		discount:  discount}
-	return oi, nil
-}
-
 func (oi *OrderItem) AddUnits(units int) error {
 	if units < 0 {
 		return &errors.OrderingDomainError{Msg: "units can't be negative"}
